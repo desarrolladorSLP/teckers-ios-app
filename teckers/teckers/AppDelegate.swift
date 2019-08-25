540fe994e0cbbafef400f20671df3f26f4d20f02
maggie97 <maggielee97@icloud.com>
2019-08-25 16:58:49 -0500

    Configuration of the buttons' color

--- teckers/teckers/AppDelegate.swift
+++ teckers/teckers/AppDelegate.swift
@@ -19,6 +19,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Use Firebase library to configure APIs
         FirebaseApp.configure()
+        UITabBar.appearance().tintColor = .purple
+        UINavigationBar.appearance().tintColor = .purple
         return true
     }
 
