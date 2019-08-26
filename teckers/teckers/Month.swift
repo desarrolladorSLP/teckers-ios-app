b64972da47a321861a570df3c23e289475de577b
chirica2501 <chirica2501@hotmail.com>
2019-08-26 13:41:40 -0500

    Refactory PrincipalNavigationItem and HomeController

---
+++ teckers/teckers/Month.swift
@@ -0,0 +1,24 @@
+//
+//  Month.swift
+//  teckers
+//
+//  Created by Ricardo Granja on 8/26/19.
+//  Copyright © 2019 Teckers. All rights reserved.
+//
+
+import Foundation
+
+enum Month: Int {
+    case January = 1
+    case February = 2
+    case March = 3
+    case April = 4
+    case May = 5
+    case June = 6
+    case July = 7
+    case August = 8
+    case September = 9
+    case Octuber = 10
+    case November = 11
+    case December = 12
+}
