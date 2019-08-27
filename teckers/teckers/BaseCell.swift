dc2eddd1c1af29e977440a3d6a3ac9022850259e
maggie97 <maggielee97@icloud.com>
2019-08-27 11:17:35 -0500

    Delete the methods setSelected and awakeFromNib

--- teckers/teckers/BaseCell.swift
+++ teckers/teckers/BaseCell.swift
@@ -13,10 +13,6 @@ class MessageCell: UITableViewCell {
     private var messages : [Message]?
     private var friend : User?
     
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-    }
-    
     func setFriend(friend : User){
         self.friend = friend
     }
@@ -28,16 +24,4 @@ class MessageCell: UITableViewCell {
     func addMessage(message : Message) {
         messages?.append(message)
     }
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-    
 }
