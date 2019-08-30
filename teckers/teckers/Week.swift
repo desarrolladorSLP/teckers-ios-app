cbf9124ee9a60221d70d363976b6c4b906e32f3a
chirica2501 <chirica2501@hotmail.com>
2019-08-30 11:20:41 -0500

    Calendar View with months move

---
+++ teckers/teckers/Week.swift
@@ -0,0 +1,19 @@
+//
+//  Week.swift
+//  teckers
+//
+//  Created by Ricardo Granja on 8/28/19.
+//  Copyright © 2019 Teckers. All rights reserved.
+//
+
+import Foundation
+
+enum Week: String {
+    case Sunday = "Sunday"
+    case Monday = "Monday"
+    case Thuesday = "Thuesday"
+    case Wednesday = "Wednesday"
+    case Thursday = "Thursday"
+    case Friday = "Friday"
+    case Saturday = "Saturday"
+}
