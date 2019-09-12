5167129f4daa55b07db536f315563933e0921934
maggie97 <maggielee97@icloud.com>
2019-09-12 12:46:32 -0500

    Add a Message Router

---
+++ teckers/teckers/MessagesRouter.swift
@@ -0,0 +1,44 @@
+//
+//  MessagesRouter.swift
+//  
+//
+//  Created by Maggie Mendez on 9/12/19.
+//
+
+import Foundation
+import Alamofire
+
+enum MessagesRouter: URLRequestConvertible {
+    
+    case getMessages
+    
+    var method: HTTPMethod {
+        switch self {
+        case .getMessages:
+            return .get
+        }
+    }
+    
+    var parameters: [String: Any] {
+        var parameters: [String: Any] = [:]
+        switch self {
+        case .getMessages:
+            parameters = [:]
+        }
+        return parameters
+    }
+    
+    var path: String {
+        switch self {
+        case .getMessages:
+            return "/api/messages"
+        }
+    }
+    
+    func asURLRequest() throws -> URLRequest {
+        let url = try RoadURL.baseURL.rawValue.asURL()
+        var urlRequest = URLRequest(url: url.appendingPathComponent(path))
+        return try URLEncoding.default.encode(urlRequest, with: parameters)
+    }
+}
+
