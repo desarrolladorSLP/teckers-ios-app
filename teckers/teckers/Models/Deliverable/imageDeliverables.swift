002ae4567f83e737bd145dd5202a03af51b227a9
maggie97 <maggielee97@icloud.com>
2019-10-15 12:20:22 -0500

    Add constants and model of Deliverables

---
+++ teckers/teckers/Models/Deliverable/imageDeliverables.swift
@@ -0,0 +1,15 @@
+//
+//  imageDeliverables.swift
+//  teckersDeliverable
+//
+//  Created by Maggie Mendez on 10/8/19.
+//  Copyright © 2019 Maggie Mendez. All rights reserved.
+//
+
+import Foundation
+enum imageDeliverables: String {
+    case circle = "circle.fill"
+    case check = "checkmark.circle.fill"
+    case blocked = "minus.circle.fill"
+    case cross = "x.circle.fill"
+}
---
+++ teckers/teckers/Models/Deliverable/statusDeliverables.swift
@@ -0,0 +1,20 @@
+//
+//  statusDeliverables.swift
+//  teckersDeliverable
+//
+//  Created by Maggie Mendez on 10/8/19.
+//  Copyright © 2019 Maggie Mendez. All rights reserved.
+//
+
+import UIKit
+
+enum statusDeliverables: Int{
+    case inProgress = 1
+    case toDo = 2
+    case readyForReview = 3
+    case accepted = 4
+    case rejected = 5
+    case blocked = 6
+    case overdue = 7
+    case none = 8
+}
