89cab65261ab4765c57d13d5aca6094f5779cde5
maggie97 <maggielee97@icloud.com>
2019-12-12 11:35:51 -0600

    Teckers by mentor

--- teckers/teckers/Controller/MainTabBarController.swift
+++ teckers/teckers/Controller/MainTabBarController.swift
@@ -14,38 +14,30 @@ class MainTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         let mainStoryboard = UIStoryboard(name: Storyboards.logedStoryboard.rawValue, bundle: Bundle.main)
-        
-        if !(roles.contains(Roles.Administrador.rawValue)) {
-        }
-        else if (roles.contains(Roles.Mentor.rawValue)) {
-            if let viewMentor = mainStoryboard.instantiateViewController(withIdentifier: Views.DeliverablesParentMentor.rawValue) as? DeliverablesTeckersController {
-                DeliverableService.getDeliverableMentor(completion: { [weak self] (deliverableArray, error) in
-                    viewMentor.teckers = deliverableArray ?? []
-                    if viewMentor.teckers.count == 1 {
-                        if let viewTecker = mainStoryboard.instantiateViewController(withIdentifier: Views.DeliverablesTecker.rawValue) as? DeliverablesController {
-                            self?.viewControllers?[1] = viewTecker
+        let roles = UserDefaults.standard.array(forKey: TokenKeys.Roles.rawValue) ?? []
+        for r in roles {
+            let storyboard: UIStoryboard = UIStoryboard(name: Storyboards.logedStoryboard.rawValue, bundle: Bundle.main)
+//            let navigation = UINavigationController()
+            if let role = r as? String{
+                switch(Roles(rawValue: role)){
+                    case .Administrador:
+//                                if let programs = storyboard.instantiateViewController(withIdentifier: Views.ProgramBatchControllerID.rawValue) as? ProgramBatchController {
+//                                    self.viewControllers?[1] = programs
+                            return
+//                                }
+                    case .Mentor:
+                        if let chooseTecker = storyboard.instantiateViewController(withIdentifier: Views.DeliverablesParentMentor.rawValue) as? DeliverablesTeckersController {
+//                                    chooseTecker.getTeckers()
+                            self.viewControllers?[1] = chooseTecker
+                            return
                         }
-                    }
-                    self?.viewControllers?[1] = viewMentor
-                })
-            }
-        }
-        else if (roles.contains(Roles.Parent.rawValue)) {
-            if let viewParent = mainStoryboard.instantiateViewController(withIdentifier: Views.DeliverablesParentMentor.rawValue) as? DeliverablesTeckersController {
-                DeliverableService.getDeliverableParent(completion: { [weak self] (deliverableArray, error) in
-                    viewParent.teckers = deliverableArray ?? []
-                    if viewParent.teckers.count == 1 {
-                        if let viewTecker = mainStoryboard.instantiateViewController(withIdentifier: Views.DeliverablesTecker.rawValue) as? DeliverablesController {
-                            self?.viewControllers?[1] = viewTecker
+                    case .Tecker:
+                        if let deliverable = storyboard.instantiateViewController(withIdentifier: Views.DeliverablesID.rawValue) as? DeliverablesController {
+                            self.viewControllers?[1] = deliverable
                         }
-                    }
-                    self?.viewControllers?[1] = viewParent
-                })
-            }
-        }
-        else if roles.contains(Roles.Tecker.rawValue) {
-            if let viewTecker = mainStoryboard.instantiateViewController(withIdentifier: Views.DeliverablesTecker.rawValue) as? DeliverablesController {
-                self.viewControllers?[1] = viewTecker
+                    default:
+                        break
+                }
             }
         }
     }
